152158dd5af8907a367db366649a964d0f409b4b
veselinr <veselin@grabber.vip>
2023-12-21 12:26:03 +0100

    remove react and jsx-runtime from build

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,12 @@ export default defineConfig({
       entry: resolve(__dirname, 'lib/main.ts'),
       formats: ['es']
     },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+      output: {
+        entryFileNames: '[name].js',
+      }
+    },
     copyPublicDir: false,
-  }
+  },
 })
